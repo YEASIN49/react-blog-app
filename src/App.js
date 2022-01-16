344aa3f5cc41a5adbd07ad56ae1149304eb8fa50
MD.YEASIN ALI <myeasin49@gmail.com>
2022-01-16 23:54:50 +0600

    responsive navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React, { useState } from 'react';
 import Navbar from "./components/Navbar";
 
 function App() {
+
+  const [isBurgerBtnOpen, setIsBurgerBtnOpen] = useState(false);
+  const [isSearchModalOpen, setIsSearchModalOpen] = useState(false);
+
+  const toggleBurgerBtn = () => {
+    const burgerButtonPrevState = isBurgerBtnOpen;
+    setIsBurgerBtnOpen(!burgerButtonPrevState);
+  }
+  const toggleSearchModal = () => {
+    const searchModalPrevState = isSearchModalOpen;
+    setIsSearchModalOpen(!searchModalPrevState);
+  }
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar
+        isBurgerBtnOpen={isBurgerBtnOpen}
+        isSearchModalOpen={isSearchModalOpen}
+        toggleSearchModal={toggleSearchModal}
+        toggleBurgerBtn={toggleBurgerBtn}
+      />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,38 @@
 import '../css/navbar.css';
 import profileImage from '../images/user-profile.jpg';
 
-const Navbar = () => {
+const Navbar = ({ isBurgerBtnOpen, toggleSearchModal, isSearchModalOpen, toggleBurgerBtn }) => {
+
+	// isBurgerBtnOpen
+
 	return (
-		<nav className='sticky'>
+		<nav className='navItemContainer sticky'>
 			<div className="navLeft logo">Blog<span>Ali</span></div>
-			<div className='navCenter'>
+			<div className={isBurgerBtnOpen ? 'navCenter navOpen' : 'navCenter navClosed'}>
 				<a href='' className='navLinks'>HOME</a>
 				<a href='' className='navLinks'>ABOUT</a>
 				<a href='' className='navLinks'>CONTACT</a>
 				<a href='' className='navLinks'>WRITE</a>
 				<a href='' className='navLinks'>LOGOUT</a>
 			</div>
-			<div className='navRight'>
+			<div className={isBurgerBtnOpen ? 'navRight' : 'navRight navClosed'}>
+				<div onClick={toggleSearchModal} className={isSearchModalOpen ? 'modalContainer' : 'modalContainer modalClose'}>
+					<form action="" className='searchForm'>
+						<input type="text" className='searchInputField' placeholder="Search.." name="search" />
+						<button type="submit" className='searchButton'><span className="material-icons searchIcon">search</span></button>
+					</form>
+				</div>
+				<button onClick={toggleSearchModal} className='searchButtonToggler'><span className="material-icons">search</span></button>
 				<img className='profileImage' src={profileImage} />
-				<span className="material-icons searchIcon">search</span>
 			</div>
-		</nav>
+			<div className={'burgerButtonContainer'} onClick={toggleBurgerBtn}>
+				<div class={isBurgerBtnOpen ? "burgerpng hamburger-btn-close" : "burgerpng"}>
+					<div class="line1"></div>
+					<div class="line2"></div>
+					<div class="line3"></div>
+				</div>
+			</div>
+		</nav >
 	);
 }
 
